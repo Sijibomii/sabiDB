2b736ad8b523b361326369633e0b558f3181dfc3
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2025-12-29 00:51:29 +0100

    tx

---
+++ sabi/crates/sabi-core/src/tx.rs
@@ -0,0 +1,29 @@
+use uuid::Uuid;
+
+/// Unique transaction identifier
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct TxId(Uuid);
+
+impl TxId {
+    pub fn new() -> Self {
+        TxId(Uuid::new_v4())
+    }
+
+    pub fn nil() -> Self {
+        TxId(Uuid::nil())
+    }
+}
+
+impl std::fmt::Display for TxId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+impl std::str::FromStr for TxId {
+    type Err = uuid::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let uuid = Uuid::parse_str(s)?;
+        Ok(TxId(uuid))
+    }
+}
